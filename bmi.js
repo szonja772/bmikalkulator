70945f46d76a2bad3c37f61455c457637f7d303f
szonja772 <72388932+szonja772@users.noreply.github.com>
2022-02-02 15:12:00 +0100

    Add files via upload
    működő változat

--- bmi.js
+++ bmi.js
@@ -1,1 +1,7 @@
 /*bmi*/
+function bmi(){
+    m=document.getElementById("magassag").value;
+    t=document.getElementById("tomeg").value;
+    e=parseFloat(t)/(parseFloat(m)*parseFloat(m));
+    document.getElementById("eredmeny").innerHTML=Math.round(100*e)/100;
+  }
